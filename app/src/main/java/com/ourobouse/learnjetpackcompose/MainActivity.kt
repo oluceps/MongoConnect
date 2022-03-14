b0b79c17e4fbcef83f2673d614af55ee1c81f4c3
Dabrit <ly9460730472580800@163.com>
2022-03-14 20:36:52 +0800

    finish main card UI

--- app/src/main/java/com/ourobouse/learnjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/ourobouse/learnjetpackcompose/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.indication
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
@@ -15,16 +16,20 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.VerticalAlignmentLine
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.ourobouse.learnjetpackcompose.ui.theme.LearnJetpackComposeTheme
 import org.litote.kmongo.KMongo
 import org.litote.kmongo.getCollection
+import org.litote.kmongo.text
 import java.net.Authenticator
+import kotlin.math.round
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,7 +41,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = Color.White
                 ) {
-                    MainSurface()
+                    Column() {
+
+                        MainSurface()
+                    }
                 }
             }
         }
@@ -44,57 +52,73 @@ class MainActivity : ComponentActivity() {
 }
 
 
-@Preview(showBackground = false)
+@Preview(showBackground = true)
 @Composable
 fun MainSurface(){
-    val buttonStatus = remember { mutableStateOf(false) }
-    Column(Modifier.padding(2.dp)) {
-        Card(shape = RoundedCornerShape(20.dp),
-            modifier = Modifier.padding(30.dp),
-        backgroundColor = MaterialTheme.colors.primary) {
-            Row(modifier = Modifier.size(width = 320.dp, height = 140.dp)) {
-                Image(painter = painterResource(id = R.drawable.ic_launcher_background),
-                    contentDescription = "test",
-                modifier = Modifier.clip(RoundedCornerShape(20.dp)).padding(20.dp))
-                Column (modifier = Modifier.padding(8.dp)){
-                    Text(text = "left")
-                    Text(text = "left down")
-                }
-                Column(modifier = Modifier.padding(2.dp)) {
-                    Spacer(modifier = Modifier.height(8.dp))
-                    OutlinedButton(onClick = { buttonStatus.value = !buttonStatus.value }) {
-                        Text(if (buttonStatus.value) "expanded" else "none")
-                    }
-
-                }
-
-
-            }
-
-        }
-        Card(shape = RoundedCornerShape(20.dp), modifier = Modifier.padding(40.dp)) {
-            Row(modifier = Modifier.padding(10.dp)) {
-                Text(text = "test")
-            }
-        }
+    val connectStatus= remember {
+        mutableStateOf(false)
     }
 
+        Card(border = ButtonDefaults.outlinedBorder,
+
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(20.dp)
+            ) {
+           Row(horizontalArrangement = Arrangement.SpaceEvenly) {
+               Column (verticalArrangement = Arrangement.Center,
+               modifier = Modifier.padding(20.dp)){
+                   Text(text = "Connection Status: ",
+                   Modifier.padding(15.dp))
+                   Text(text = "Authentication Type: ",
+                       Modifier.padding(15.dp))
+                   Text(text = "User: ",
+                   Modifier.padding(15.dp))
+
+                   }
+               Column() {
+                   Image(painter = if (connectStatus.value) painterResource(id = R.drawable.ic_baseline_check_circle_24)
+                   else painterResource(id = R.drawable.ic_baseline_highlight_off_24),
+                       contentDescription = "No connected",
+                       modifier = Modifier
+                           .size(width = 180.dp, height = 180.dp)
+                           .padding(20.dp))
 
 
-}
+               }
 
+
+               }
+
+           }
+
+
+
+
+
+
+            }
+
+
+
+/*
 data class QQleaks(val phone: String, val QQ: String)
 data class Weiboleaks(val Phone: String, val uid: String)
-val client = KMongo.createClient() //get com.mongodb.MongoClient new instance
-val database = client.getDatabase("SEL") //normal java driver usage
-val colQQleaks = database.getCollection<QQleaks>() //KMongo extension method
-val colWeiboLeaks = database.getCollection<Weiboleaks>()
 
 class MongoDB(val dbAdress: String,
               val userName: String,
               val passWord: String,
               val cryptoType: String){
+
+
+    val client = KMongo.createClient() //get com.mongodb.MongoClient new instance
+    val database = client.getDatabase("SEL") //normal java driver usage
+    val colQQleaks = database.getCollection<QQleaks>() //KMongo extension method
+    val colWeiboLeaks = database.getCollection<Weiboleaks>()
     fun Connect(dbAdress: String, userName: String, passWord: String, cryptoType: String){
+
 
     }
 }
+
+ */
